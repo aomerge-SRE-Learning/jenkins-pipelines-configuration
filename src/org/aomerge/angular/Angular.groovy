57301efa220722ecfe70af9170ebe93a7d103758
aomerge <codenode.ortega@gmail.com>
2025-12-08 14:50:07 -0600

    Fix Angular class structure and package naming

---
+++ src/org/aomerge/angular/Angular.groovy
@@ -0,0 +1,9 @@
+package org.aomerge.angular
+
+class Angular implements Serializable { 
+    static void runAngular(script){
+        script.echo "🔧 Ejecutando Stage Angular desde src/org/aomerge/angular/"
+        script.echo "🅰️ Compilando proyecto Angular..."
+        script.echo "📦 Generando build de producción..."
+    }
+}

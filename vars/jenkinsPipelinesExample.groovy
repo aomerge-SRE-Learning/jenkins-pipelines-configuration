d12f79fad4ca3a95d4360308b32ef472ddc41177
aomerge <codenode.ortega@gmail.com>
2025-12-08 14:35:02 -0600

    Add ExtraStages class in src/ and fix import

--- vars/jenkinsPipelinesExample.groovy
+++ vars/jenkinsPipelinesExample.groovy
@@ -26,7 +26,7 @@ def call(Map config = [:]) {
             stage('Extra') {
                 steps {
                     script {
-                        org.Angular.ExtraStages.runExtraSteps(this)
+                        ExtraStages.runExtraSteps(this)
                     }
                 }
             }

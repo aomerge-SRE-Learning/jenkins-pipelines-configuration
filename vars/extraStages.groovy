dddddd4c1ace4f2aeb3d4d2de7e4692b2749ab61
aomerge <codenode.ortega@gmail.com>
2025-12-08 14:25:03 -0600

    Add extraStages function in vars/

---
+++ vars/extraStages.groovy
@@ -0,0 +1,10 @@
+/**
+ * Stages adicionales que pueden ser consumidos por otros pipelines
+ */
+def call() {
+    echo "🔧 Ejecutando Stage Extra 1"
+    echo "📦 Preparando entorno..."
+    
+    echo "🔧 Ejecutando Stage Extra 2"
+    echo "✨ Finalizando tareas adicionales"
+}
